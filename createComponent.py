51458cfce5877ab87560e9506e6a5e0a116c9478
yashrajendra <youngster.yash@gmail.com>
2021-06-08 19:36:54 +0530

    Create Component of rule

---
+++ createComponent.py
@@ -0,0 +1,72 @@
+from requests.api import request
+from requests.models import Response
+import config
+import pandas as pd
+import httplib2
+import requests
+import json
+from pandas.io.json import json_normalize
+
+_header = {"Accept": "application/vnd.api+json;revision=1",
+           "Content-Type": "application/vnd.api+json;revision=1",
+           "Authorization": "Bearer " + config.access_token,
+           "X-Api-Key": config.config['apiKey'],
+           'X-Gw-Ims-Org-Id': config.config["orgId"]
+           }
+
+_companyId = requests.get(
+    'https://reactor.adobe.io/companies?filter[name]=CONTAINS PLC', headers=_header, verify=False).json()['data'][0]['id']
+
+
+def getPropertyId(config, access_token, companyId):
+    response = requests.get("https://reactor.adobe.io/companies/"+str(companyId) +
+                            "/properties?filter[name]=CONTAINS sky", headers=_header, verify=False)
+    return response.json()['data'][0]['id']
+
+
+
+def createComponent(config, access_token):
+    # https://experience.adobe.com/#/@accenture-partner/data-collection/client/companies/CO268e14e0982744c98e52417ad4ed6833/properties/PR23e1ae6d96634c5dab7e01c113397dac/rules/RL8ade93b528144b0685df3ca658831bfc
+    # https://experience.adobe.com/#/@accenture-partner/data-collection/client/companies/CO268e14e0982744c98e52417ad4ed6833/properties/PR23e1ae6d96634c5dab7e01c113397dac/extensions/EXc26c6bcee0764457bea1fa8581f9928c
+    propertyId = getPropertyId(config, access_token, _companyId)
+    print(propertyId)
+    post_body = """ {
+  "data": {
+    "attributes": {
+      "delegate_descriptor_id": "core::events::click",
+      "name": "My Example Click Event"
+     
+    },
+    "relationships": {
+      "extension": {
+        "data": {
+          "id": "EXc26c6bcee0764457bea1fa8581f9928c",
+          "type": "extensions"
+        }
+      },
+      "rules": {
+        "data": [
+          {
+            "id": "RL8ade93b528144b0685df3ca658831bfc",
+            "type": "rules"
+          }
+        ]
+      }
+    },
+    "type": "rule_components"
+  }
+} """
+
+  
+    to_python = json.loads(post_body)
+                 
+    print("Helllooo  ooo   ")
+    response = requests.post("https://reactor.adobe.io/properties/"+propertyId +"/rule_components", data=json.dumps(to_python), headers=_header, verify=False)
+
+    if(response.status_code == 201):
+          print("Congratulations! Your Rule component is Created Successfully")
+    else:
+          print("Oops! Error Occured. Please check the error here:", response)
+            
+            
+createComponent(config.config, config.access_token)
--- main.py
+++ main.py
@@ -4,27 +4,29 @@ import config
 import pandas as pd
 import httplib2
 import requests
-import json  
+import json
 from pandas.io.json import json_normalize
 
 _header = {"Accept": "application/vnd.api+json;revision=1",
-               "Content-Type": "application/vnd.api+json;revision=1",
-               "Authorization": "Bearer " + config.access_token,
-               "X-Api-Key": config.config['apiKey'],
-               'X-Gw-Ims-Org-Id': config.config["orgId"]
-               }
+           "Content-Type": "application/vnd.api+json;revision=1",
+           "Authorization": "Bearer " + config.access_token,
+           "X-Api-Key": config.config['apiKey'],
+           'X-Gw-Ims-Org-Id': config.config["orgId"]
+           }
 
-_companyId=requests.get('https://reactor.adobe.io/companies?filter[name]=CONTAINS PLC', headers=_header, verify=False).json()['data'][0]['id']
+_companyId = requests.get(
+    'https://reactor.adobe.io/companies?filter[name]=CONTAINS PLC', headers=_header, verify=False).json()['data'][0]['id']
 
 
-def getPropertyId(config,access_token,companyId):
-    response=requests.get("https://reactor.adobe.io/companies/"+str(companyId)+"/properties?filter[name]=CONTAINS sky",headers=_header, verify=False)
+def getPropertyId(config, access_token, companyId):
+    response = requests.get("https://reactor.adobe.io/companies/"+str(companyId) +
+                            "/properties?filter[name]=CONTAINS sky", headers=_header, verify=False)
     return response.json()['data'][0]['id']
 
 
-def createRule(config,access_token):
-    propertyId=getPropertyId(config,access_token,_companyId)
-    post_body="""
+def createRule(config, access_token):
+    propertyId = getPropertyId(config, access_token, _companyId)
+    post_body = """
         {
       "data": {
     "attributes": {
@@ -35,11 +37,14 @@ def createRule(config,access_token):
     }
     """
     to_python = json.loads(post_body)
-    response = requests.post("https://reactor.adobe.io/properties/"+propertyId+"/rules",data=json.dumps(to_python),headers=_header,verify=False)
-    if(response.status_code==201):
+    response = requests.post("https://reactor.adobe.io/properties/"+propertyId +
+                             "/rules", data=json.dumps(to_python), headers=_header, verify=False)
+    if(response.status_code == 201):
         print("Congratulations! Your Rule is Created Successfully")
     else:
         print("Oops! Error Occured. Please check the error here:", response)
 
+
 createRule(config.config, config.access_token)
+
 
