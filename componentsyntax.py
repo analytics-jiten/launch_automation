c954a9f76a0dff859975643f8ecf0bbaa0ffc599
yashrajendra <youngster.yash@gmail.com>
2021-06-11 15:10:24 +0530

    creating event action condition using excel sheet

--- componentsyntax.py
+++ componentsyntax.py
@@ -176,3 +176,47 @@ r"""{
 		"type": "rule_components"
 	}
 }"""
+
+
+def createComponentAction(config, access_token):
+    # https://experience.adobe.com/#/@accenture-partner/data-collection/client/companies/CO268e14e0982744c98e52417ad4ed6833/properties/PR23e1ae6d96634c5dab7e01c113397dac/rules/RL8ade93b528144b0685df3ca658831bfc
+    # https://experience.adobe.com/#/@accenture-partner/data-collection/client/companies/CO268e14e0982744c98e52417ad4ed6833/properties/PR23e1ae6d96634c5dab7e01c113397dac/extensions/EXc26c6bcee0764457bea1fa8581f9928c
+    propertyId = getPropertyId(config, access_token, _companyId)
+    print(propertyId)
+    post_body = r"""{
+	"data": {
+			
+			"attributes": {
+			"delegate_descriptor_id": "core::actions::custom-code",
+			"name": "Core - Custom Code",
+			"settings": "{\"source\": \"console\",\"language\":\"html\"}"
+		},
+		"relationships": {
+			"extension": {
+				"data": {
+					"id": "EXc26c6bcee0764457bea1fa8581f9928c",
+					"type": "extensions"
+				}
+			},
+			"rules": {
+				"data": [{
+					"id": "RL8ade93b528144b0685df3ca658831bfc",
+					"type": "rules"
+				}]
+			}
+		},
+		"type": "rule_components"
+	}
+}"""
+    to_python = json.loads(post_body)
+    response = requests.post("https://reactor.adobe.io/properties/"+propertyId +"/rule_components", data=json.dumps(to_python), headers=_header, verify=False)
+
+    if(response.status_code == 201):
+          print("Congratulations! Your Rule component is Created Successfully")
+    else:
+          print("Oops! Error Occured. Please check the error here:", response.json)
+    jsonResponse = response.json()
+    print("Entire JSON response")
+    print(jsonResponse)
+ 
+
